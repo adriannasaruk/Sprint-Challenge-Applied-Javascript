97b036d74c433c9574358339f68988756a2e8763
Adrian Nasaruk <adriannasaruk@gmail.com>
2020-02-14 09:01:03 -1000

    finished project

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,50 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+.then (response => {
+    console.log(response)
+    const objects = Object.keys(response.data.articles)
+    console.log (objects)
+    objects.forEach (item=>{
+        console.log (response.data.articles[item])
+        response.data.articles[item].forEach (element =>{
+            console.log(element)
+            const newcard = createCard(element)
+            cardscontainer.appendChild(newcard)
+        })
+    })    
+})
+.catch(error => {
+    console.log("The data was not returned", error);
+  });
+
+
+
+  function createCard (element) {
+      const card = document.createElement("div")
+      const headline = document.createElement("div")
+      const author = document.createElement("div")
+      const imagecont = document.createElement("div")
+      const img = document.createElement("img")
+      const span = document.createElement("span")
+
+      card.classList.add("card")
+      headline.classList.add("headline")
+      headline.textContent = element.headline
+      author.classList.add("author")
+      imagecont.classList.add("img-container")
+      img.setAttribute("src", element.authorPhoto)
+      span.textContent = "By " + element.authorName
+
+      card.appendChild(headline)
+      card.appendChild(author)
+      author.appendChild(imagecont)
+      imagecont.appendChild(img)
+      author.appendChild(span)
+      return card
+  }
+
+  const cardscontainer = document.querySelector(".cards-container")
+  
--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    const header = document.createElement("div")
+    const span1 = document.createElement("span")
+    const title = document.createElement("h1")
+    const span2 = document.createElement ("span")
+
+
+    header.classList.add("header")
+    span1.classList.add("date")
+    span1.textContent = "SMARCH 28, 2019"
+    title.textContent = "Lambda Times"
+    span2.classList.add("temp")
+    span2.textContent = "98°"
+
+    header.appendChild(span1)
+    header.appendChild(title)
+    header.appendChild(span2)
+
+    return header
+}
+
+const head = document.querySelector(".header-container")
+head.appendChild(Header())
+
+   
